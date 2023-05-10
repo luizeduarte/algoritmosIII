cc82688f8e1daa125ed9b9842b873ba50d375193
Luize Duarte <luize.duarte@ufpr.br>
2023-05-10 17:10:45 -0300

    Update insere_avl.h

--- insere_avl.h
+++ insere_avl.h
@@ -2,8 +2,6 @@
 #define _INSERE_AVL_H_
 #include "manipula_avl.h"
 
-struct nodo_avl* aloca_nodo(int valor);	//aloca memoria de um nodo e inicializa seus valores
-
 //recebe a raiz e insere um nodo com o valor passado, balanceando a arvore e retornando a raiz da mesma
 struct nodo_avl* insere_nodo(struct nodo_avl* nodo, int valor);	
 
